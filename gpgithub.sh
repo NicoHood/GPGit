ef0376fa45df78fc1cca4b50e0eb53471276dccf
NicoHood <git@nicohood.de>
2017-01-03 14:17:51 +0100

    Tabs to spaces + debug removal

--- gpgithub.sh
+++ gpgithub.sh
@@ -71,53 +71,53 @@ ARGS=( "$@" )
 # Check if messages are to be printed using color
 unset ALL_OFF BOLD BLUE GREEN RED YELLOW
 if [[ -t 2 ]]; then
-	# prefer terminal safe colored and bold text when tput is supported
-	if tput setaf 0 &>/dev/null; then
-		ALL_OFF="$(tput sgr0)"
-		BOLD="$(tput bold)"
-		BLUE="${BOLD}$(tput setaf 4)"
-		GREEN="${BOLD}$(tput setaf 2)"
-		RED="${BOLD}$(tput setaf 1)"
-		YELLOW="${BOLD}$(tput setaf 3)"
-	else
-		ALL_OFF="\e[1;0m"
-		BOLD="\e[1;1m"
-		BLUE="${BOLD}\e[1;34m"
-		GREEN="${BOLD}\e[1;32m"
-		RED="${BOLD}\e[1;31m"
-		YELLOW="${BOLD}\e[1;33m"
-	fi
+    # prefer terminal safe colored and bold text when tput is supported
+    if tput setaf 0 &>/dev/null; then
+        ALL_OFF="$(tput sgr0)"
+        BOLD="$(tput bold)"
+        BLUE="${BOLD}$(tput setaf 4)"
+        GREEN="${BOLD}$(tput setaf 2)"
+        RED="${BOLD}$(tput setaf 1)"
+        YELLOW="${BOLD}$(tput setaf 3)"
+    else
+        ALL_OFF="\e[1;0m"
+        BOLD="\e[1;1m"
+        BLUE="${BOLD}\e[1;34m"
+        GREEN="${BOLD}\e[1;32m"
+        RED="${BOLD}\e[1;31m"
+        YELLOW="${BOLD}\e[1;33m"
+    fi
 fi
 readonly ALL_OFF BOLD BLUE GREEN RED YELLOW
 
 msg() {
-	local mesg=$1; shift
-	printf "${GREEN}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+    local mesg=$1; shift
+    printf "${GREEN}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
 }
 
 msg2() {
-	local mesg=$1; shift
-	printf "${BLUE}  ->${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+    local mesg=$1; shift
+    printf "${BLUE}  ->${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
 }
 
 plain() {
-	local mesg=$1; shift
-	printf "${BOLD}    ${mesg}${ALL_OFF}\n" "$@" >&2
+    local mesg=$1; shift
+    printf "${BOLD}    ${mesg}${ALL_OFF}\n" "$@" >&2
 }
 
 warning() {
-	local mesg=$1; shift
-	printf "${YELLOW}==> WARNING:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+    local mesg=$1; shift
+    printf "${YELLOW}==> WARNING:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
 }
 
 error() {
-	local mesg=$1; shift
-	printf "${RED}==> ERROR:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+    local mesg=$1; shift
+    printf "${RED}==> ERROR:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
 }
 
 info() {
-	local mesg=$1; shift
-	printf "${YELLOW}[!]:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+    local mesg=$1; shift
+    printf "${YELLOW}[!]:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
 }
 
 gpgithub_yesno() {
@@ -140,13 +140,13 @@ fi
 
 # Print help
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-	plain "Usage: ${PROGNAME} <tag>" 1>&2
+    plain "Usage: ${PROGNAME} <tag>" 1>&2
     exit 0
 fi
 
 # Check if inside a git folder
 if [[ "$(git rev-parse --is-inside-work-tree)" != "true" ]]; then
-	error "Not a git repository."
+    error "Not a git repository."
 fi
 
 # Set default values in config array
@@ -155,7 +155,7 @@ config=(
     [TAG]="$1"
     [OUTPUT]="$(git rev-parse --show-toplevel)/archive"
     [USERNAME]="$(git config user.name)"
-	[EMAIL]="$(git config user.email)"
+    [EMAIL]="$(git config user.email)"
     [PROJECT]="$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')"
     [GPG]="$(git config --global user.signingkey)"
 )
@@ -164,58 +164,55 @@ config=(
 msg "1. Generate new GPG key"
 ################################################################################
 
-# TODO temporary
-export GNUPGHOME="$(mktemp -d)"
-
 # Check for existing key
-if [[ ! -z "${config[GPG]}" ]]; then
+if [[ -z "${config[GPG]}" ]]; then
     if gpg --list-secret-keys | grep uid | grep -v -q revoked; then
         error "GPG seems to be already configured on your system but git is not."
-		plain "Please use gpg --list-secret-keys to show existing keys."
+        plain "Please use gpg --list-secret-keys to show existing keys."
         plain "Afterwards set the key with git config --global user.signingkey <key>."
         plain "See the readme for mor information."
         exit 1
     else
         plain "Generating an RSA 4096 GPG key for ${config[USERNAME]} <${config[EMAIL]}> valid for 3 years."
-		gpgithub_yesno
+        gpgithub_yesno
 
-		# ECC command (currently not supported by Github)
-		#gpg --quick-generate-key "testuser (comment) <name@mail.com>" future-default default 3y
+        # ECC command (currently not supported by Github)
+        #gpg --quick-generate-key "testuser (comment) <name@mail.com>" future-default default 3y
 
-		# RSA command
-		# https://www.gnupg.org/documentation/manuals/gnupg/Unattended-GPG-key-generation.html
-		# gpg: revocation certificate stored as '/tmp/tmp.81v03YSxmI/openpgp-revocs.d/F4EDF85EFF03D746D17094D3C28B8F6BCCDF8671.rev'
-		GPGFINGERPRINT="$(gpg --batch --generate-key <( cat <<- EOF
-			Key-Type: RSA
-			Key-Length: 4096
-			Key-Usage: cert sign auth
-			Subkey-Type: RSA
-			Subkey-Length: 4096
-			Subkey-Usage: encrypt
-			Name-Real: ${config[USERNAME]}
-			#Name-Comment: Generated with gpgit
-			Name-Email: ${config[EMAIL]}
-			Expire-Date: 3y
-			# Preferences: TODO https://security.stackexchange.com/questions/82216/how-to-change-default-cipher-in-gnupg-on-both-linux-and-windows
-			%ask-passphrase
-			%echo We need to generate a lot of random bytes. It is a good idea to perform
-			%echo some other action (type on the keyboard, move the mouse, utilize the
-			%echo disks) during the prime generation; this gives the random number
-			%echo generator a better chance to gain enough entropy.
-			%commit
-			%echo Key generation finished
-			EOF
-		) 2>&1 | tee -a /dev/fd/2 | grep "revocation certificate stored as " \
-			   | sed 's,.*/\(.*\).rev.*,\1,')"
+        # RSA command
+        # https://www.gnupg.org/documentation/manuals/gnupg/Unattended-GPG-key-generation.html
+        # gpg: revocation certificate stored as '/tmp/tmp.81v03YSxmI/openpgp-revocs.d/F4EDF85EFF03D746D17094D3C28B8F6BCCDF8671.rev'
+        GPGFINGERPRINT="$(gpg --batch --generate-key <( cat << EOF
+            Key-Type: RSA
+            Key-Length: 4096
+            Key-Usage: cert sign auth
+            Subkey-Type: RSA
+            Subkey-Length: 4096
+            Subkey-Usage: encrypt
+            Name-Real: ${config[USERNAME]}
+            #Name-Comment: Generated with gpgit
+            Name-Email: ${config[EMAIL]}
+            Expire-Date: 3y
+            # Preferences: TODO https://security.stackexchange.com/questions/82216/how-to-change-default-cipher-in-gnupg-on-both-linux-and-windows
+            %ask-passphrase
+            %echo We need to generate a lot of random bytes. It is a good idea to perform
+            %echo some other action (type on the keyboard, move the mouse, utilize the
+            %echo disks) during the prime generation; this gives the random number
+            %echo generator a better chance to gain enough entropy.
+            %commit
+            %echo Key generation finished
+EOF
+        ) 2>&1 | tee -a /dev/fd/2 | grep "revocation certificate stored as " \
+               | sed 's,.*/\(.*\).rev.*,\1,')"
 
-		# Print new fingerprint
-		echo "Your new GPG fingerprint is: $GPGFINGERPRINT"
-		gpg --list-secret-keys --keyid-format LONG "${GPGFINGERPRINT}"
+        # Print new fingerprint
+        echo "Your new GPG fingerprint is: $GPGFINGERPRINT"
+        gpg --list-secret-keys --keyid-format LONG "${GPGFINGERPRINT}"
 
-		# 3.1 Configure git GPG key
-		msg2 "For the simplicity of the script git will be configured with the new key now."
-		plain "This equals to step: 3.1 Configure git GPG key"
-		gpgithub_yesno
+        # 3.1 Configure git GPG key
+        msg2 "For the simplicity of the script git will be configured with the new key now."
+        plain "This equals to step: 3.1 Configure git GPG key"
+        gpgithub_yesno
         git config --global user.signingkey "${GPGFINGERPRINT}"
     fi
 fi
@@ -270,11 +267,11 @@ git pull --tags
 
 # Check if tag exists
 if ! git tag | grep "^${config[TAG]}$" -q; then
-	# Check if every added file has been commited
-	if ! git diff --cached --exit-code; then
-	    warning "You have added new changes but did not commit them yet."
-		gpgithub_yesno
-	fi
+    # Check if every added file has been commited
+    if ! git diff --cached --exit-code; then
+        warning "You have added new changes but did not commit them yet."
+        gpgithub_yesno
+    fi
 
     # Create new tag if not existant
     plain "Creating signed tag ${config[TAG]} and pushing it to the remote git."
